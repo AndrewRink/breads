0e919d14fe46f9e9b1ad6ae99def89305ed5ce38
Andrew <andrewcrink@gmail.com>
2022-11-29 18:43:02 -0500

    added bread controller

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ require('dotenv').config()
 
 const app = express()
 
+// http verbs/methods: get, post, put ,patch, delete
+app.get('/', (req, res) => {
+    res.send('<h1>Hello!</h1>')
+})
+
+
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
