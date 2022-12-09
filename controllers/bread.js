76e1fcf3d56fd7d50d2b9e63625f7fb7b46449ef
Andrew <andrewcrink@gmail.com>
2022-12-08 20:45:37 -0500

    connected to Mongo

--- controllers/bread.js
+++ controllers/bread.js
@@ -2,9 +2,10 @@ const router = require('express').Router()
 const Bread = require('../models/bread')
 
 //GET: all the bread
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const bread = await Bread.find()
     res.render('index', {
-        breads: Bread
+        breads: bread
     })
 })
 
@@ -13,25 +14,58 @@ router.get('/new', (req,res) => {
     res.render('new')
 })
 
-//get bread by its index
-router.get('/:index', (req,res) => {
+//GET edit bread page
+router.get('/:index/edit', (req,res) => {
     const { index } = req.params
+    const bread = Bread[index]
+    res.render('edit', {
+        bread,
+        index
+    })
+})
+
+//get bread by its index
+router.get('/:id', async(req,res) => {
+    const { id } = req.params
+    const bread = await Bread.findById(id)
     res.render('show', {
-        bread: Bread[index]
+        bread
     })
 })
 
 //post: create a bread
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
     const { hasGluten,image } = req.body
+    if (!image) req.body.image = undefined
+    if (hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    await Bread.create(req.body)
+    res.redirect('/breads')
+})
+
+router.put('/:index', (req,res) =>{
+    const { index } = req.params
+    const { image, hasGluten } = req.body
     if (!image) req.body.image = 'https://alexandracooks.com/wp-content/uploads/2021/01/simplesourdoughhalved.jpg'
     if (hasGluten === 'on') {
         req.body.hasGluten = true
     } else {
         req.body.hasGluten = false
     }
-    Bread.push(req.body)
-    res.redirect('/breads')
+
+    Bread[index] = req.body
+    res.redirect(`/breads/${index}`)
+})
+
+//DELETE a bread
+router.delete('/:index',(req,res) => {
+    const { index } = req.params
+    const numIndex = Number(index)
+    Bread.splice(numIndex, 1)
+    res.status(303).redirect('/breads')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 require('dotenv').config()
 
 const breadRoutes = require('./controllers/bread')
+
+
 //intialize
 const app = express()
 
 // MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 //routes
 app.use('/breads', breadRoutes)
+
+mongoose.set('strictQuery', true);
+//connect to database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello!</h1>')
